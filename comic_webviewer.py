8c238588d0592d8945ff344c7ea250b5aca259fd
yellowbigbird <yellowbigbird@qq.com>
2018-11-20 17:26:05 +0800

    load config file. (#10)

--- comic_webviewer.py
+++ comic_webviewer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-import argparse, os, subprocess, tempfile, shutil, argparse
-from cwebviewer import create_app
+import argparse
+
+from cwebviewer.config import load_json_file
+from cwebviewer.manage import create_app
+
 
 def positive_type(x):
     x = int(x)
@@ -12,25 +15,32 @@ def positive_type(x):
 
 def main():
     parse = argparse.ArgumentParser(description='comic webviewer')
-    parse.add_argument('--debug', '-d', action='store_true', help='debug mode')
-    parse.add_argument('--sort', '-s', choices=['name', 'time', 'size', 'random'], default='name', help='archive display order')
-    parse.add_argument('--reverse', '-r', action='store_true', help='reversed sort order')
-    parse.add_argument('--webp-quality', '-c', type=int, default=5, help='webp quaility [0-100], default: 5')
+    parse.add_argument('--debug', '-d', action='store_true', default=argparse.SUPPRESS, help='debug mode')
+    parse.add_argument('--sort', '-s', choices=['name', 'time', 'size', 'random'], default=argparse.SUPPRESS, help='archive display order')
+    parse.add_argument('--reverse', '-r', action='store_true', default=argparse.SUPPRESS, help='reversed sort order')
+    parse.add_argument('--webp-quality', '-c', type=int, default=argparse.SUPPRESS, help='webp quaility [0-100], default: 5')
     parse.add_argument('--webp-preset', choices=[
                             'default', 'photo', 'picture',
                             'drawing', 'icon', 'text'
-        ], default='drawing', help='webp preset (default/photo/picture/drawing/icon/text), default: drawing')
-    parse.add_argument('--disable-webp', action='store_true', help='disable webp mode')
-    parse.add_argument('--port', '-p', type=int, default=5001, help='port to listen on, default: 5001')
-    parse.add_argument('--address', '-a', default='127.0.0.1', help='listen address, default: 127.0.0.1')
-    parse.add_argument('--img-per-page', type=positive_type, default=20, help='specify how many image(s) in one view')
-    parse.add_argument('--archive-per-page', type=positive_type, default=20, help='specify how many archive(s) in one subindex')
-    parse.add_argument("directories", nargs="+", help="directory names to serve")
-    config = parse.parse_args()
-    d = { k.upper() : v for k, v in vars(config).items() }
-    app = create_app(d)
-    app.logger.warning("listen on %s:%d" % (config.address, config.port))
-    app.run(debug=config.debug, host=config.address, port=config.port)
+        ], default=argparse.SUPPRESS, help='webp preset (default/photo/picture/drawing/icon/text), default: drawing')
+    parse.add_argument('--disable-webp', action='store_true', default=argparse.SUPPRESS, help='disable webp mode')
+    parse.add_argument('--port', '-p', type=int, default=argparse.SUPPRESS, help='port to listen on, default: 5001')
+    parse.add_argument('--address', '-a', default=argparse.SUPPRESS, help='listen address, default: 127.0.0.1')
+    parse.add_argument('--img-per-page', type=positive_type, default=argparse.SUPPRESS, help='specify how many image(s) in '
+                                                                                     'one view')
+    parse.add_argument('--archive-per-page', type=positive_type, default=argparse.SUPPRESS, help='specify how many archive(s) in one subindex')
+    parse.add_argument("directories", nargs="*", default=argparse.SUPPRESS, help="directory names to serve")
+
+    config_namespace = parse.parse_args()
+    config_input = { k.upper() : v for k, v in vars(config_namespace).items() }
+
+    config_dict = load_json_file('config.json')
+    config_dict.update(config_input)
+
+    app = create_app(config_dict)
+
+    app.logger.warning("listen on %s:%d" % (config_dict['ADDRESS'], config_dict['PORT']))
+    app.run(debug=config_dict['DEBUG'], host=config_dict['ADDRESS'], port=config_dict['PORT'])
 
 if __name__ == "__main__":
     main()
---
+++ cwebviewer/manage.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, render_template, request, make_response, url_for, redirect, Blueprint
+from flask import current_app as app
+from itertools import islice
+
+from cwebviewer import archive
+from cwebviewer.config import load_json_file
+from .views import cwebviewer_pages
+from .consts import *
+
+
+def create_app(config=None):
+    app = Flask(__name__)
+    app.secret_key = os.urandom(16)
+
+    # app.config.from_mapping(
+    #         PORT = 5001,
+    #         ADDRESS = "127.0.0.1",
+    #         WEBP=True,
+    #         WEBP_QUALITY=5,
+    #         WEBP_PRESET='drawing',
+    #         DISABLE_WEBP=False,
+    #         SORT='name',
+    #         REVERSE=False,
+    #         IMG_PER_PAGE=20,
+    #         ARCHIVE_PER_PAGE=20,
+    #         DIRECTORIES=[ '.' ],
+    # )
+
+    if config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(config)
+
+    with app.app_context():
+        app.config['WEBP'] = False
+        if app.config['DISABLE_WEBP'] is False and CWEBP_PATH is not None:
+            app.logger.warning("webp enabled, quality: %d, preset: %s" % (app.config['WEBP_QUALITY'], app.config['WEBP_PRESET']))
+            app.config['WEBP'] = True
+        app.logger.warning("sorted by %s order%s" % (app.config['SORT'], ", descending" if app.config['REVERSE'] else ""))
+        app.repo = [[dirname, os.stat(dirname).st_mtime, archive.load(dirname, app.config['SORT'], app.config['REVERSE'])] for dirname in app.config['DIRECTORIES']]
+        for e in app.repo:
+            app.logger.warning("Directory %s: %d archvies loaded." % (e[DIRNAME], len(e[ARCHIVE])))
+        app.register_blueprint(cwebviewer_pages)
+        @app.template_filter('strip_path')
+        def strip_path(s, subdir):
+            return os.path.relpath(s, subdir)
+        @app.context_processor
+        def inject_config():
+            return dict(basename=os.path.basename, islice=islice, app=app, DIRNAME=DIRNAME, MTIME=MTIME, ARCHIVE=ARCHIVE)
+
+    return app
+
+# vim: set tabstop=4 sw=4 expandtab:
