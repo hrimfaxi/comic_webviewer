1cdbef83646d9dbec4ad199d8d9c0c636f84b993
hrimfaxi <outmatch@gmail.com>
2018-12-20 10:23:05 +0800

    back to subindex after completed a comic instead of archive page
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

---
+++ cwebviewer/static/js/link_width.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" language="JavaScript">
+    $(document).ready(function() {
+        width = $(window).width();
+        height = $(window).height();
+        // console.log('width ' + width + ' height ' + height);
+        $('a.imglink').each(function(o) {
+            $(this).attr("href", $(this).attr("href") + '&width=' + Math.max(width, height));
+        });
+    });
+</script>
