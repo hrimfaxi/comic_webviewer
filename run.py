d63fa4a1f69f999417e205b9477cf3a98c7d3755
hrimfaxi <outmatch@gmail.com>
2018-12-26 17:02:33 +0800

    delete useless run.py
    Signed-off-by: hrimfaxi <outmatch@gmail.com>

--- run.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# coding: utf-8
-
-from cwebviewer import create_app
-
-app = create_app()
-app.run(debug=True, port=app.config['PORT'], host=app.config['ADDRESS'])
-
-# vim: set tabstop=4 sw=4 expandtab:
